4417b61eecb53c361eae5685ce54e564a60dbd8e
neelchavan <neelchavan9@gmail.com>
2020-04-20 11:35:37 +0530

    dictionary

---
+++ Dictionary.py
@@ -0,0 +1,8 @@
+#here we are using dictionaries to give the meaning of some words to the user 
+
+d1 = {'mes':'In english it means \'more\'','que':'In english it means \'Than\'','un':'In english it means \'A\'','club':'In english it means \'Club\''}
+#Enter the word to get the meaning of it
+Word = input("Enter the word\n",)
+print(d1[Word])
+
+
