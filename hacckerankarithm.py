7c5f4d44f64a1da732ebdf89d3201ba6db52f370
neelchavan <neelchavan9@gmail.com>
2020-05-01 22:24:19 +0530

    hacckerank arithmetic

---
+++ hacckerankarithm.py
@@ -0,0 +1,10 @@
+a=int(input('enter first no\n'))
+b=int(input('enter second no\n'))
+print(a//b) #floor division
+print(a/b)
+
+'''
+"//"
+Floor Division - The division of operands where the result is the quotient in which the digits after the decimal point are removed.
+ But if one of the operands is negative, the result is floored, i.e., rounded away from zero (towards negative infinity) −
+ '''
