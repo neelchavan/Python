2cd2f394cbf1137df66cae3183090f0869867822
neelchavan <neelchavan9@gmail.com>
2020-05-02 14:05:49 +0530

    hacckerank print function

---
+++ printfunction.py
@@ -0,0 +1,14 @@
+#Hacckerank print function
+n = int(input())
+for i in range (1,n+1):
+    print(i,end="")
+
+
+
+
+#print table
+num = int(input())
+for i in range (2,11):
+    print(num*i)
+    
+
