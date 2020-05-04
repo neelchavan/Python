c0806e4a21ac987c754bee458c2339faa768fe3c
neelchavan <neelchavan9@gmail.com>
2020-05-04 19:50:28 +0530

    hacckerank- whats your name

--- whatsyourname.py
+++ whatsyourname.py
@@ -4,6 +4,8 @@ def print_full_name(a,b):
     print('Hello',a,b,'You just delved into python.')
 print_full_name(input(),input())
 
+               # OR
+
 def print_full_name(a, b):
     print('Hello {} {}! You just delved into python.'.format(a,b))
 print_full_name('ross','taylor')    
