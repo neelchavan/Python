d3a2dbceebffe7cb0e43dc6435cbaf2fe3528162
neelchavan <neelchavan9@gmail.com>
2020-05-21 12:41:01 +0530

    hackerrank problem

---
+++ text wrap.py
@@ -0,0 +1,20 @@
+import textwrap
+string=input()
+n=int(input())
+result = textwrap.fill(string,n)
+print(result)
+
+
+
+
+import textwrap
+
+def wrap(string, max_width):
+    x=textwrap.fill(string,max_width)
+    return x
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
+
