26ec0e1d23f7c24ebd99ee5a047ef92ed9c48aa1
neelchavan <neelchavan9@gmail.com>
2020-06-23 18:23:21 +0530

    problem solving

---
+++ PS Birthday cake candles.py
@@ -0,0 +1,13 @@
+n=int(input())
+for arr in range(n):
+    arr = list(map(int,input().rstrip().split()))
+    print(arr.count(max(arr)))
+    break
+'''
+4
+3 2 1 3'''
+
+'''
+def birthdayCakeCandles(ar):
+    
+    return(ar.count(max(ar)))'''
